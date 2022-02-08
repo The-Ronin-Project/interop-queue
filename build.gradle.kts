aff6e9776699851649489486750804639a58b9a2
Josh Smith <43484632+arssycro@users.noreply.github.com>
2022-02-08 17:30:30 -0600

    INT-363: Use axion-release for versions (#8)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-    id("com.projectronin.interop.gradle.base") version "1.0.0-SNAPSHOT"
-    id("com.projectronin.interop.gradle.junit") version "1.0.0-SNAPSHOT" apply false
-    id("com.projectronin.interop.gradle.ktorm") version "1.0.0-SNAPSHOT" apply false
-    id("com.projectronin.interop.gradle.mockk") version "1.0.0-SNAPSHOT" apply false
-    id("com.projectronin.interop.gradle.publish") version "1.0.0-SNAPSHOT" apply false
-    id("com.projectronin.interop.gradle.spring") version "1.0.0-SNAPSHOT" apply false
+    id("com.projectronin.interop.gradle.base")
+    id("com.projectronin.interop.gradle.junit") apply false
+    id("com.projectronin.interop.gradle.ktorm") apply false
+    id("com.projectronin.interop.gradle.mockk") apply false
+    id("com.projectronin.interop.gradle.publish") apply false
+    id("com.projectronin.interop.gradle.spring") apply false
 }
 
 subprojects {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,16 @@ for (project in rootProject.children) {
 }
 
 pluginManagement {
+    val interopGradleVersion: String by settings
+    plugins {
+        id("com.projectronin.interop.gradle.base") version interopGradleVersion
+        id("com.projectronin.interop.gradle.junit") version interopGradleVersion
+        id("com.projectronin.interop.gradle.ktorm") version interopGradleVersion
+        id("com.projectronin.interop.gradle.mockk") version interopGradleVersion
+        id("com.projectronin.interop.gradle.publish") version interopGradleVersion
+        id("com.projectronin.interop.gradle.spring") version interopGradleVersion
+    }
+
     repositories {
         maven {
             url = uri("https://maven.pkg.github.com/projectronin/package-repo")
@@ -17,6 +27,7 @@ pluginManagement {
                 password = System.getenv("PACKAGE_TOKEN")
             }
         }
+        mavenLocal()
         mavenCentral()
         gradlePluginPortal()
     }
