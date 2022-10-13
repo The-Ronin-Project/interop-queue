dd4c9dfad43cfd75a4f6aa8aa93c18f31f3f99d0
Josh Smith <43484632+arssycro@users.noreply.github.com>
2022-10-12 21:20:58 -0500

    Update to 2.1.2 of interop-gradle plugins (#55)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,12 +11,12 @@ for (project in rootProject.children) {
 
 pluginManagement {
     plugins {
-        id("com.projectronin.interop.gradle.base") version "2.1.1"
-        id("com.projectronin.interop.gradle.junit") version "2.1.1"
-        id("com.projectronin.interop.gradle.publish") version "2.1.1"
-        id("com.projectronin.interop.gradle.spring") version "2.1.1"
-        id("com.projectronin.interop.gradle.spring-boot") version "2.1.1"
-        id("com.projectronin.interop.gradle.version") version "2.1.1"
+        id("com.projectronin.interop.gradle.base") version "2.1.2"
+        id("com.projectronin.interop.gradle.junit") version "2.1.2"
+        id("com.projectronin.interop.gradle.publish") version "2.1.2"
+        id("com.projectronin.interop.gradle.spring") version "2.1.2"
+        id("com.projectronin.interop.gradle.spring-boot") version "2.1.2"
+        id("com.projectronin.interop.gradle.version") version "2.1.2"
     }
 
     repositories {
